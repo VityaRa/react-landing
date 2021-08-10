a42c3245642f66fbaf4f8f68b1863d7e70b847cb
VityaRa <blackops2in@mail.ru>
2021-08-10 23:06:49 +0300

    fix index.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import Root from './components/root/Root';
-import './index.css'
 import { GlobalStyle } from "./styles/global"
 
 ReactDOM.render(
